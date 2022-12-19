e5d580b448c9deaa5c106ad785668d243f356f9e
Juan Devia <infojuandevia@gmail.com>
2022-12-19 17:34:55 -0500

    Add files via upload

---
+++ docs/index.js
@@ -0,0 +1,23 @@
+const img = document.querySelector("img");
+
+img.addEventListener("click", e => {
+  const src = img.getAttribute("src");
+  if (src === "/images/img01.webp") {
+    img.setAttribute("src", "/images/img02.webp");
+  } else if (src === "/images/img02.webp") {
+    img.setAttribute("src", "/images/img03.webp");
+  } else {
+    img.setAttribute("src", "/images/img01.webp");
+  }
+});
+
+// img.onclick = function () {
+//   const imgSrc = img.getAttribute("src");
+//   if (imgSrc === "/images/img01.webp") {
+//     img.setAttribute("src", "/images/img02.webp");
+//   } else if (imgSrc === "/images/img02.webp") {
+//     img.setAttribute("src", "/images/img03.webp");
+//   } else {
+//     img.setAttribute("src", "/images/img01.webp");
+//   }
+// };
